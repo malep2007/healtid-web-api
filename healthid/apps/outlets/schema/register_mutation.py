521a7a40b800040d5d282700bc5565958d42f956
habibaudu <auduhabib1990@gmail.com>
2019-03-31 17:35:13 +0100

    - update register_mutation.py to add some input checks - update test_register.py to include tests [#163849535]

--- healthid/apps/outlets/schema/register_mutation.py
+++ healthid/apps/outlets/schema/register_mutation.py
@@ -5,6 +5,13 @@ from healthid.apps.outlets.models import Register
 from healthid.apps.outlets.schema.register_schema import RegisterType
 
 
+class RegisterInput(graphene.InputObjectType):
+
+    id = graphene.String()
+    name = graphene.String()
+
+
+
 class CreateRegister(graphene.Mutation):
     """
     This Creates a register
@@ -16,20 +23,25 @@ class CreateRegister(graphene.Mutation):
         outlet_id = graphene.Int()
         receipt_id = graphene.String()
 
+
+
     @login_required
     @master_admin_required
     def mutate(self, info, **kwargs):
-        try:
-            register = Register()
-            for(k, v) in kwargs.items():
-                setattr(register, k, v)
-            register.save()
-        except Exception as e:
-            raise Exception(f'Something went wrong {e}')
+        name = kwargs.get('name')
+        if name:
+                try:
+                    register = Register()
+                    for(k, v) in kwargs.items():
+                        setattr(register, k, v)
+                    register.save()
+                except Exception as e:
+                    raise Exception(f'Something went wrong {e}')
 
-        return CreateRegister(
-            register=register
-        )
+                return CreateRegister(
+                    register=register
+                )
+        return "Enter Name field"
 
 
 
@@ -37,27 +49,50 @@ class UpdateRegister(graphene.Mutation):
     """
     This Updates a register
     """
-    register = graphene.Field(RegisterType)
 
     class Arguments:
         id = graphene.Int()
-        name = graphene.String()
+        input = RegisterInput(required=True)
 
-    ok = graphene.Boolean()
+    errors = graphene.List(graphene.String)
+    message = graphene.List(graphene.String)
+    success = graphene.Boolean()
+    register = graphene.Field(RegisterType)
 
+    @staticmethod
     @login_required
     @master_admin_required
-    def mutate(self, info, name, **kwargs):
-        ok=False
-        id = kwargs.get('id')
-        register = Register.objects.get(pk=id)
-        if register:
-            ok = True
-            register.name = name
-            register.save()
-            return UpdateRegister(ok=ok, register=register)
-        return UpdateRegister(ok=ok, register=None)
+    def mutate(root, info, id, input=None):
+        success = False
+        if id is None:
+            errors = ["id", "Register Id is empty"]
+            return UpdateRegister(success, errors=errors)
 
+        register_instance = Register.objects.get(pk=id)
+
+        if register_instance is None:
+            errors = ["Register", "Register does not exist"]
+            return UpdateRegister(success, errors=errors)
+
+        if input.name != "":
+            try:
+                success = True
+                register_instance.name = input.name
+                if register_instance:
+                   register_instance.save()
+                   message = [
+                        f"Successfully updated {register_instance} to {input.name}"
+                    ]
+                   return UpdateRegister(
+                        success=success, register=register_instance, message=message
+                    )
+                return UpdateRegister(success=success, register=None)
+            except Exception as e:
+                errors = ["Something went wrong: {}".format(e)]
+                return UpdateRegister(success=False, errors=errors)
+
+        errors = ["name", "Name Field is empty"]
+        return UpdateRegister(success=False, errors=errors)
 
 
 class DeleteRegister(graphene.Mutation):
@@ -65,28 +100,31 @@ class DeleteRegister(graphene.Mutation):
     This Deletes a register
     """
     id = graphene.Int()
+    success = graphene.String()
 
 
     class Arguments:
         id = graphene.Int()
 
-    ok = graphene.Boolean()
-
     @login_required
     @master_admin_required
     def mutate(self, info, id):
-        ok = False
         register = Register.objects.get(pk=id)
         if register:
             register.delete()
             return DeleteRegister(
-            ok=True)
+            success="Register was deleted successfully")
 
-        return DeleteRegister(ok=ok)
+        return DeleteRegister(success="Error No register by that ID")
 
 
 class Mutation(graphene.ObjectType):
     create_register = CreateRegister.Field()
     delete_register = DeleteRegister.Field()
     update_register = UpdateRegister.Field()
+
+
+
+
+
 
